983eba783fca0caf98614c8551643c4cb0943b4f
Emmanuel Akuffo <eawayo@st.ug.edu.gh>
2024-05-18 18:17:11 +0000

    formatArrayStrings

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -12,3 +12,22 @@ function processArray(arr) {
 
 const arr = processArray([1,3,7,6]);
 console.log(arr);
+
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, index) => {
+      const num = numbers[index];
+      if (num % 2 === 0) {
+          return str.toUpperCase(); 
+      } else {
+          return str.toLowerCase();
+      }
+  });
+}
+
+
+const inputArray = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(inputArray);
+const strings = ["hello", "world", "this", "is", "example"];
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+
+console.log(formattedStrings); 
